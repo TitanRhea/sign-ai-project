eb7aa5f18dc25d0b8a36b85fc8c99f29e1487045
TitanRhea <myprojectsignai@gmail.com>
2026-04-18 21:57:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,6 @@ CONFIDENCE_THRESHOLD = 0.40
 last_spoken_word = None
 last_spoken_time = 0
 
-pending_gesture = None
-gesture_start_time = 0
-
 @app.route('/')
 def index():
     return "SignAI Brain is LIVE!"
@@ -35,7 +32,6 @@ def index():
 @socketio.on('process_landmarks')
 def handle_landmarks(data):
     global current_candidate, consecutive_frames, last_spoken_word, last_spoken_time
-    global pending_gesture, gesture_start_time
     
     if model is None: return
     
@@ -57,61 +53,34 @@ def handle_landmarks(data):
     prediction_prob = probabilities[best_class_index]
 
     # =========================================================
-    # --- Η ΤΕΛΕΙΑ ΛΟΓΙΚΗ ΤΗΣ ΟΛΓΑΣ ---
+    # --- Η ΑΠΟΛΥΤΗ ΓΕΩΜΕΤΡΙΚΗ ΛΟΓΙΚΗ (Geofencing) ---
     # =========================================================
     wrist_x = raw_landmarks[0]['x']
-    wrist_y = raw_landmarks[0]['y']
     
-    index_up = raw_landmarks[8]['y'] < (raw_landmarks[5]['y'] - 0.04)
-    middle_down = raw_landmarks[12]['y'] > raw_landmarks[10]['y']
-    ring_down = raw_landmarks[16]['y'] > raw_landmarks[14]['y']
-    pinky_down = raw_landmarks[20]['y'] > raw_landmarks[18]['y']
+    # 1. Είναι ο Δείκτης σηκωμένος; (Η άκρη του (8) πιο ψηλά από την κλείδωσή του (6))
+    index_up = raw_landmarks[8]['y'] < raw_landmarks[6]['y']
     
-    is_fist_base = middle_down and ring_down and pinky_down
-    is_high = wrist_y < 0.80 
+    # 2. Είναι το χέρι στο πλάι; (Απέχει από το κέντρο της οθόνης)
     is_side = abs(wrist_x - 0.5) > 0.15 
     
+    # Αν το AI προβλέψει μία από τις 3 "προβληματικές" λέξεις, παίρνουμε τον έλεγχο:
     if active_now in ["efharisto", "kalimera", "kalo_mesimeri"]:
         if not is_side:
-            # Κέντρο = Ευχαριστώ
+            # Αν το χέρι είναι στο ΚΕΝΤΡΟ (Στέρνο), είναι 100% ΕΥΧΑΡΙΣΤΩ
             active_now = "efharisto"
             prediction_prob = 0.99
-            pending_gesture = None
         else:
-            # Στο πλάι (άρα Καλημέρα ή Καλό μεσημέρι)
-            if is_high and is_fist_base:
-                if pending_gesture is None:
-                    pending_gesture = "checking"
-                    gesture_start_time = time.time()
-            
-            if pending_gesture == "checking":
-                if index_up:
-                    # Σήκωσε δείκτη! -> Καλημέρα
-                    active_now = "kalimera"
-                    prediction_prob = 0.99
-                    pending_gesture = None
-                elif not is_high or not is_side:
-                    # Το χέρι χάθηκε προς τα κάτω ή έφυγε, χωρίς να δούμε δείκτη! -> Καλό Μεσημέρι
-                    if "kalo_mesimeri" != last_spoken_word or (time.time() - last_spoken_time > 2.0):
-                        socketio.emit('new_sign', {'word': "kalo_mesimeri"})
-                        last_spoken_word = "kalo_mesimeri"
-                        last_spoken_time = time.time()
-                    
-                    pending_gesture = None
-                    current_candidate = None
-                    consecutive_frames = 0
-                    return # Τερματισμός, το στείλαμε κατευθείαν στην οθόνη!
-                else:
-                    # Σιγή ασυρμάτου, περιμένουμε την επόμενη κίνηση
-                    active_now = "noise"
-                    prediction_prob = 0.0
-                    
-                    if (time.time() - gesture_start_time) > 1.5:
-                        pending_gesture = None
+            # Αν το χέρι είναι στο ΠΛΑΙ (πρόσωπο/ώμος)...
+            if index_up:
+                # ...και έχει δείκτη όρθιο = ΚΑΛΗΜΕΡΑ
+                active_now = "kalimera"
+                prediction_prob = 0.99
             else:
-                active_now = "noise"
+                # ...και ΔΕΝ έχει δείκτη όρθιο = ΚΑΛΟ ΜΕΣΗΜΕΡΙ
+                active_now = "kalo_mesimeri"
+                prediction_prob = 0.99
 
-    # ΕΙΔΙΚΟΣ ΚΑΝΟΝΑΣ ΓΙΑ ΓΕΙΑ (Παραμένει ίδιος)
+    # ΕΙΔΙΚΟΣ ΚΑΝΟΝΑΣ ΓΙΑ ΓΕΙΑ (Παραμένει άθικτος)
     if active_now not in ["efharisto", "kalimera", "kalo_mesimeri"]:
         if prediction_prob < CONFIDENCE_THRESHOLD:
             if active_now == "geia" and prediction_prob >= 0.30:
